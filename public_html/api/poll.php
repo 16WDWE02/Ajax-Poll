a4a2b31f04ec1456f65e4e2d4d58c6b11c4ff870
Richard Hpa <richard.hpa@hotmail.com>
2016-08-31 11:54:00 +1200

    added ajax-poll project

---
+++ public_html/api/poll.php
@@ -0,0 +1,75 @@
+<?php 
+//inlcude our config file
+include('../../config.inc.php');
+
+//Capture the Vote
+$vote = strtolower($_GET['vote']);
+
+// Acceptable values
+$validVotes = ['yes', 'no'];
+
+//Validate the vote
+$isValid = in_array($vote, $validVotes);
+
+if($isValid == false){
+	$message = [
+		'status' => false,
+		'message' => 'Vote is invalid'
+	];
+
+	//Convert the message into JSON
+	$message-> json_encode($message);
+
+	//Prepare the header
+	header('Content-Type: application/json');
+
+	echo $message;
+
+	exit();
+}
+
+//Get the users IP address
+//For testing I am commenting this line out
+// $ipaddress = $_SERVER['REMOTE_ADDR'];
+
+//For testing, create random number to pretend we are using different IP Addresses
+$ipaddress = rand() . "\n";
+
+//Connect to database
+$dbc = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
+//Prepare the insert query
+$sql = "INSERT INTO vote VALUES (NULL, '$vote', '$ipaddress')";
+
+//Run the query
+$dbc->query($sql);
+
+if($dbc->affected_rows == 1){
+	//Vote sent
+
+	//Get the vote summary
+	$sql = "SELECT SUM( (CASE WHEN vote = 'yes' THEN 1 ELSE 0 END) ) AS TotalYes, SUM( (CASE WHEN vote = 'no' THEN 1 ELSE 0 END) ) AS TotalNo FROM vote";
+
+	$result = $dbc->query($sql);
+
+	//Convert into an associative array
+	$result = $result->fetch_assoc();
+
+	$message = [
+		'status' => true,
+		'message' => 'Thank you for your vote',
+		'totalYes' => $result['TotalYes'],
+		'totalNo' => $result['TotalNo']
+	];
+
+	//Prepare the header
+	header('Content-Type: application/json');
+
+	echo json_encode($message);
+}
+
+
+
+
+
+
